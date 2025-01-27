a56e2de03814978225f6db9bf00713e19720c00e
Spencer Judge <spencer@temporal.io>
2025-01-27 15:34:05 -0800

    Make and issue scaling decisions

--- service/matching/matching_engine.go
+++ service/matching/matching_engine.go
@@ -109,6 +109,8 @@ type (
 		workerVersionCapabilities *commonpb.WorkerVersionCapabilities
 		deploymentOptions         *deploymentpb.WorkerDeploymentOptions
 		forwardedFrom             string
+		// TODO: Should we set this in forwarded polls as well, or is local wait time sufficient?
+		localPollStartTime time.Time
 	}
 
 	userDataUpdate struct {
@@ -2304,6 +2306,8 @@ func (e *matchingEngineImpl) pollTask(
 		return nil, false, err
 	}
 
+	pollMetadata.localPollStartTime = e.timeSource.Now()
+
 	// We need to set a shorter timeout than the original ctx; otherwise, by the time ctx deadline is
 	// reached, instead of emptyTask, context timeout error is returned to the frontend by the rpc stack,
 	// which counts against our SLO. By shortening the timeout by a very small amount, the emptyTask can be
@@ -2500,6 +2504,9 @@ func (e *matchingEngineImpl) createPollWorkflowTaskQueueResponse(
 	if task.backlogCountHint != nil {
 		response.BacklogCountHint = task.backlogCountHint()
 	}
+	if task.pollerScalingDecision != nil {
+		response.PollerScalingDecision = task.pollerScalingDecision
+	}
 	return response
 }
 
@@ -2560,6 +2567,7 @@ func (e *matchingEngineImpl) createPollActivityTaskQueueResponse(
 		HeartbeatDetails:            historyResponse.HeartbeatDetails,
 		WorkflowType:                historyResponse.WorkflowType,
 		WorkflowNamespace:           historyResponse.WorkflowNamespace,
+		PollerScalingDecision:       task.pollerScalingDecision,
 	}
 }
 
